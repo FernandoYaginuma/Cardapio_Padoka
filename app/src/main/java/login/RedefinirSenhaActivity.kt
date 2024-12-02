50d9b2436c64b568877ae0dc90f6c624bae013b2
PedroAOC20 <pedrodeveloperpyc@gmail.com>
2024-12-02 11:26:43 -0300

    Inserido o campo "nome" e realocado o arquivo activity do redefinir senha para pasta login.

--- app/src/main/java/login/RedefinirSenhaActivity.kt
+++ app/src/main/java/login/RedefinirSenhaActivity.kt
@@ -1,10 +1,11 @@
-package com.example.cardapio_padoka
+package login
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cardapio_padoka.R
 
 class RedefinirSenhaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
