5abb3abdf8e07788280189b0a68c68d5edd4cb57
FernandoYaginuma <fernandoyaginuma@gmail.com>
2024-12-03 03:42:58 -0300

    item detalhe novo

--- app/src/main/java/View/ItemDetalheActivity.kt
+++ app/src/main/java/View/ItemDetalheActivity.kt
@@ -1,18 +1,17 @@
 package View
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
 import com.example.cardapio_padoka.databinding.ActivityItemDetalheBinding
+import com.bumptech.glide.Glide
 
 class ItemDetalheActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityItemDetalheBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityItemDetalheBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -25,21 +24,36 @@ class ItemDetalheActivity : AppCompatActivity() {
         binding.itemDescription.text = itemDescricao
         binding.itemPrice.text = "R$ %.2f".format(itemPreco)
 
+
         itemImagemUrl?.let {
             Glide.with(this)
                 .load(it)
                 .into(binding.itemImage)
         }
 
+        binding.numberPicker.minValue = 1
+        binding.numberPicker.maxValue = 100
+        binding.numberPicker.value = 1
+
         binding.addToCartButton.setOnClickListener {
-            adicionarAoPedido(itemNome, itemPreco)
+            val quantidade = binding.numberPicker.value
+            adicionarAoPedido(itemNome, itemPreco, quantidade)
+            val intent = Intent(this, CardapioActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.btnBack.setOnClickListener {
+            val intent = Intent(this, CardapioActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 
-    private fun adicionarAoPedido(nome: String, preco: Double) {
+    private fun adicionarAoPedido(nome: String, preco: Double, quantidade: Int) {
         Toast.makeText(
             this,
-            "Item \"$nome\" adicionado ao pedido por R$ %.2f".format(preco),
+            "$quantidade x \"$nome\" adicionado ao pedido por R$ %.2f".format(preco * quantidade),
             Toast.LENGTH_SHORT
         ).show()
     }
